07b04090c6bb68e3600daf0b30cce8c25d7602fd
anzerwall <anzerwall@gmail.com>
2016-08-31 18:02:18 +0800

    冲突测试

--- clientDemo/src/pages/Demand/Dating.vue
+++ clientDemo/src/pages/Demand/Dating.vue
@@ -16,7 +16,7 @@ module.exports = {
                 color: '#EC407A',
                 sex:2,
                 likeNum:8,
-                joinNum:1,
+                joinNum:3,
                 liked:true,
                 time: 1472581345635
             },{
