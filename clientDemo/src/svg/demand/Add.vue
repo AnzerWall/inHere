0a854c7586315fccce3dde78a187e505b7c60e10
Kenefe Li <kenefe.li@gmail.com>
2016-08-31 17:32:17 +0800

    补回消失的文件

---
+++ clientDemo/src/svg/demand/Add.vue
@@ -0,0 +1,20 @@
+<template>
+    <?xml version="1.0" encoding="UTF-8"?>
+<svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 39.1 (31720) - http://www.bohemiancoding.com/sketch -->
+    <title>Page 1</title>
+    <desc>Created with Sketch.</desc>
+    <defs></defs>
+    <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="task" transform="translate(-333.000000, -43.000000)" fill="#000000">
+            <g id="Group-6">
+                <g id="search" transform="translate(24.000000, 37.000000)">
+                    <g id="Group">
+                        <polygon id="Page-1" points="325 15.1275168 318.127517 15.1275168 318.127517 22 315.872483 22 315.872483 15.1275168 309 15.1275168 309 12.8724832 315.872483 12.8724832 315.872483 6 318.127517 6 318.127517 12.8724832 325 12.8724832"></polygon>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+</template>
