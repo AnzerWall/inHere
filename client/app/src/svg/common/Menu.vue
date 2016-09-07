1ed2c572afab9f5315d591da0ca7fae0e079bff9
Kenefe Li <kenefe.li@gmail.com>
2016-09-07 11:15:01 +0800

    Menu.svg 颜色fixed

--- client/app/src/svg/common/Menu.vue
+++ client/app/src/svg/common/Menu.vue
@@ -1,18 +1,16 @@
 <template>
-    <?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <svg width="23px" height="18px" viewBox="0 0 23 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <!-- Generator: Sketch 39.1 (31720) - http://www.bohemiancoding.com/sketch -->
-    <title>Group</title>
+    <title>Page 1</title>
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd" stroke-linecap="round">
-        <g id="有问必答详细页" transform="translate(-329.000000, -44.000000)"  stroke-width="2">
-            <g id="Group-7">
-                <g id="Group" transform="translate(330.000000, 44.000000)">
-                    <path d="M0.42,1 L20.5974925,1" id="Line"></path>
-                    <path d="M0.42,9 L20.5974925,9" id="Line-Copy-2"></path>
-                    <path d="M0.42,17 L20.5974925,17" id="Line-Copy-3"></path>
-                </g>
+    <g id="Symbols" stroke="none" stroke-width="1" fill-rule="evenodd">
+        <g id="me_list" transform="translate(-195.000000, -49.000000)">
+            <g id="Page-1" transform="translate(195.000000, 49.000000)">
+                <path d="M21.2,2 L1,2 C0.4,2 0,1.6 0,1 C0,0.4 0.4,0 1,0 L21.2,0 C21.8,0 22.2,0.4 22.2,1 C22.2,1.6 21.8,2 21.2,2" id="Fill-1"></path>
+                <path d="M21.2,10 L1,10 C0.4,10 0,9.6 0,9 C0,8.4 0.4,8 1,8 L21.2,8 C21.8,8 22.2,8.4 22.2,9 C22.2,9.6 21.8,10 21.2,10" id="Fill-3"></path>
+                <path d="M21.2,18 L1,18 C0.4,18 0,17.6 0,17 C0,16.4 0.4,16 1,16 L21.2,16 C21.8,16 22.2,16.4 22.2,17 C22.2,17.6 21.8,18 21.2,18" id="Fill-5"></path>
             </g>
         </g>
     </g>
