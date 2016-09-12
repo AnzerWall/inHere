34e0b8ebcb1eabb36bae9b27aff092dd112e6f36
ni_menhao <ni_menhao@sina.com>
2016-09-12 11:56:50 +0800

    吐槽+问答业务代码

--- java/inHere-java/src/main/java/com/inHere/dao/LabelMapper.java
+++ java/inHere-java/src/main/java/com/inHere/dao/LabelMapper.java
@@ -1,17 +1,26 @@
 package com.inHere.dao;
 
+import java.util.List;
+
 import com.inHere.entity.Label;
 
 public interface LabelMapper {
-    int deleteByPrimaryKey(Integer id);
+	int deleteByPrimaryKey(Integer id);
 
-    int insert(Label record);
+	int insert(Label record);
 
-    int insertSelective(Label record);
+	int insertSelective(Label record);
 
-    Label selectByPrimaryKey(Integer id);
+	Label selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(Label record);
+	int updateByPrimaryKeySelective(Label record);
 
-    int updateByPrimaryKey(Label record);
+	int updateByPrimaryKey(Label record);
+
+	/**
+	 * 获取最火的标签列表
+	 * 
+	 * @return
+	 */
+	public List<Label> selectHotLabel();
 }
--- java/inHere-java/src/main/java/com/inHere/service/impl/AskReplyServiceImpl.java
+++ java/inHere-java/src/main/java/com/inHere/service/impl/AskReplyServiceImpl.java
@@ -1,12 +1,58 @@
 package com.inHere.service.impl;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.inHere.dao.AskReplyMapper;
+import com.inHere.dao.LabelMapper;
+import com.inHere.dto.ParamsListDto;
+import com.inHere.entity.AskReply;
+import com.inHere.entity.Label;
 import com.inHere.service.AskReplyService;
 
 @Service
 public class AskReplyServiceImpl implements AskReplyService {
-	
-	
-	
+
+	Logger log = Logger.getLogger(getClass());
+
+	@Autowired
+	private AskReplyMapper askReplyMapper;
+
+	@Autowired
+	private LabelMapper labelMapper;
+
+	/**
+	 * 获取吐槽+问答的列表
+	 * 
+	 * @param params
+	 * @return
+	 */
+	public JSONObject getList(ParamsListDto params) {
+		JSONObject data = new JSONObject();
+		log.info("进入AskReplyServiceImpl.getList()");
+		// AskReply列表参数格式化
+		List<AskReply> items = askReplyMapper.selectByParams(params);
+		JSONArray skReplyArray = new JSONArray();
+		for( AskReply tmp : items ){
+			skReplyArray.add(tmp);
+		}
+		data.put("list", skReplyArray);
+		
+		// Label列表参数格式化
+		List<Label> labels = labelMapper.selectHotLabel();
+		JSONArray lableArray = new JSONArray();
+		for (Label tmp : labels) {
+			lableArray.add(tmp.toMap());
+		}
+		data.put("labels", lableArray);
+		
+		log.info(data.toJSONString());
+		return null;
+	}
+
 }
