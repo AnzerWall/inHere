ac0fb139a219e51aa4088f2e7090a7612fb2a179
ni_menhao <ni_menhao@sina.com>
2016-09-19 09:13:58 +0800

    完善发布接口api文档，添加错误编码分类

--- java/inHere-java/src/main/java/com/inHere/dto/ReturnBaseDto.java
+++ java/inHere-java/src/main/java/com/inHere/dto/ReturnBaseDto.java
@@ -20,32 +20,36 @@ public class ReturnBaseDto<T> {
         return code;
     }
 
-    public void setCode(Integer code) {
+    public ReturnBaseDto<T> setCode(Integer code) {
         this.code = code;
+        return this;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public ReturnBaseDto<T> setStatus(String status) {
         this.status = status;
+        return this;
     }
 
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
+    public ReturnBaseDto<T> setMessage(String message) {
         this.message = message;
+        return this;
     }
 
     public T getData() {
         return data;
     }
 
-    public void setData(T data) {
+    public ReturnBaseDto<T> setData(T data) {
         this.data = data;
+        return this;
     }
 
     @Override
--- java/inHere-java/src/main/java/com/inHere/validator/DemandValidator.java
+++ java/inHere-java/src/main/java/com/inHere/validator/DemandValidator.java
@@ -38,24 +38,30 @@ public class DemandValidator extends BaseValidator {
      * @return
      */
     public ReturnBaseDto<JSONObject> createDemandItem(HttpServletRequest request) {
-        ReturnBaseDto<JSONObject> result = new ReturnBaseDto<JSONObject>();
+        // 多部分解析器
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
 
         log.info("进入到--->创建一个需求的参数校验");
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        ReturnBaseDto<JSONObject> result = new ReturnBaseDto<>();
+        result.setCode(Code.InputErr.getCode());
+        result.setStatus(Code.InputErr.getStatus());
+
+        // 错误标识, flag == false 时有不满足条件存在
+        boolean flag = true;
+
         // 检查form中是否有enctype="multipart/form-data"
-        boolean flag = multipartResolver.isMultipart(request);
+        flag = multipartResolver.isMultipart(request);
+        if (!flag) return result.setMessage("参数格式有错");
 
-        // 将request变成多部分request
+        // 将request变成多部分Request
         MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-        String ext_type_str = multiRequest.getParameter("ext_type");
-        flag = ext_type_str != null;
 
-        if (!flag) {
-            result.setCode(Code.InputErr.getCode());
-            result.setStatus(Code.InputErr.getStatus());
-            result.setMessage("请填写完整信息");
-            return result;
-        }
+        // ext_type 参数检验
+        String ext_type_str = multiRequest.getParameter("ext_type");
+        if (ext_type_str != null && !"".equals(ext_type_str.trim()))
+            flag = ext_type_str.matches("^[0-9]*$");
+        else flag = false;
+        if (!flag) return result.setMessage("参数格式有错");
 
         return null;
     }
