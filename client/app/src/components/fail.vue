79c2e6db3e82bd01d0cbf8c627f0de0e972d473a
Kenefe Li <kenefe.li@gmail.com>
2016-10-10 09:24:21 +0800

    加载失败

---
+++ client/app/src/components/fail.vue
@@ -0,0 +1,39 @@
+<template lang="jade">
+    .fail
+        .fail-content
+            sad-svg
+            .fail-text {{text||'加载失败'}}    
+</template>
+
+<script>
+import sadSvg from '../svg/common/sad.vue'
+module.exports = {
+    components:{
+        sadSvg
+    },
+    props:{
+        text:String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.fail {
+    width:170px;
+    height:170px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(213,213,213,.5);
+    border-radius: 8px;
+    .fail-content {
+        .fail-text {
+            text-align: center;
+            color: rgba(121,121,121,1);
+            padding-top:20px;
+        }
+    }
+}
+
+</style>
---
+++ client/app/src/svg/common/sad.vue
@@ -0,0 +1,25 @@
+<template>
+    <?xml version="1.0" encoding="UTF-8"?>
+    <svg width="90px" height="90px" viewBox="0 0 90 90" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <!-- Generator: Sketch Beta 40.2 (33825) - http://www.bohemiancoding.com/sketch -->
+        <title>Page 1</title>
+        <desc>Created with Sketch Beta.</desc>
+        <defs></defs>
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="有诺必行首页" transform="translate(-143.000000, -279.000000)">
+                <g id="task_card-copy" transform="translate(103.000000, 253.000000)">
+                    <g id="Page-1" transform="translate(40.000000, 26.000000)">
+                        <path d="M89.414,44.707 C89.414,69.398 69.398,89.414 44.707,89.414 C20.016,89.414 0,69.398 0,44.707 C0,20.016 20.016,0 44.707,0 C69.398,0 89.414,20.016 89.414,44.707" id="Fill-1" fill="#D8D8D8"></path>
+                        <path d="M27.0236,65.3959 C29.4556,59.7859 36.5366,55.7249 44.8976,55.7249 C53.2586,55.7249 60.3406,59.7859 62.7716,65.3959 L27.0236,65.3959 Z" id="Fill-3" fill="#4F4F4F"></path>
+                        <path d="M27.0236,65.3959 C29.4556,59.7859 36.5366,55.7249 44.8976,55.7249 C53.2586,55.7249 60.3406,59.7859 62.7716,65.3959 L27.0236,65.3959 L27.0236,65.3959 Z" id="Stroke-5" stroke="#4F4F4F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M21.0707,29.0712 L32.9767,40.9772" id="Stroke-7" stroke="#4F4F4F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M32.9764,29.0712 L21.0704,40.9772" id="Stroke-9" stroke="#4F4F4F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M32.9109,29.1368 L21.0049,41.0428" id="Stroke-11" stroke="#4F4F4F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M56.8191,29.0712 L68.7251,40.9772" id="Stroke-13" stroke="#4F4F4F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M68.7248,29.0712 L56.8188,40.9772" id="Stroke-15" stroke="#4F4F4F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+</template>
