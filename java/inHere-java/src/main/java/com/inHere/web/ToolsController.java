cb3f1465e9d4f65b40dd3ea495901cd28b06ed35
ni_menhao <ni_menhao@sina.com>
2016-10-10 09:54:02 +0800

    添加登出、修改单一设备登陆

--- java/inHere-java/src/main/java/com/inHere/web/ToolsController.java
+++ java/inHere-java/src/main/java/com/inHere/web/ToolsController.java
@@ -1,11 +1,14 @@
 package com.inHere.web;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONArray;
 import com.inHere.annotation.Authorization;
 import com.inHere.annotation.CurrentToken;
+import com.inHere.constant.Code;
 import com.inHere.dto.ReturnBaseDto;
 import com.inHere.entity.Token;
+import com.inHere.service.ToolsService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +21,23 @@ public class ToolsController {
 
     Logger log = Logger.getLogger(getClass());
 
+    @Autowired
+    private ToolsService toolsService;
+
     /**
      * 获取工具列表
      */
     @Authorization
     @RequestMapping(path = "/tools", method = RequestMethod.GET)
-    public ReturnBaseDto<JSONObject> tools(@CurrentToken Token token) {
-        ReturnBaseDto<JSONObject> result = new ReturnBaseDto<>();
+    public ReturnBaseDto<JSONArray> tools(@CurrentToken Token token) {
 
+        ReturnBaseDto<JSONArray> result = new ReturnBaseDto<>();
+
+        JSONArray items = toolsService.getList(token.getSchool_id());
+
+        result.setCode(Code.Success.getCode());
+        result.setStatus(Code.Success.getStatus());
+        result.setData(items);
         return result;
     }
 
