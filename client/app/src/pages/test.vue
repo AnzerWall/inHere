92f0fc377e625eb0e36d974924e99e302de1fdd1
Kenefe Li <kenefe.li@gmail.com>
2016-10-11 17:31:48 +0800

    修改密码弹窗

--- client/app/src/pages/test.vue
+++ client/app/src/pages/test.vue
@@ -1,133 +1,134 @@
 <template>
-<div class="test">
-  <div @click="clickTag()">{{tag || '选择标签'}}</div>
-  <button @click="showNoti('alert')">alert</button>
-  <button @click="showNoti('confirm')">confirm</button>
-  <button @click="showNoti('warning')">warning</button>
-  <button @click="showNoti('noti')">noti</button>
-  <tag-editor v-ref:tag-editor :tags.sync="tags" :tag.sync="tag" :editable="true" :show.sync="tagShow"></tag-editor>
-  <noti v-ref:noti></noti>
-  <fail text="玲玉不让你看"></fail>
-</div>
+    <div class="test">
+        <div @click="clickTag()">{{tag || '选择标签'}}</div>
+        <button @click="showChangePassword()">修改密码</button>
+        <br>
+        <button @click="showNoti('alert')">alert</button>
+        <button @click="showNoti('confirm')">confirm</button>
+        <button @click="showNoti('warning')">warning</button>
+        <button @click="showNoti('noti')">noti</button>
+        <tag-editor v-ref:tag-editor :tags.sync="tags" :tag.sync="tag" :editable="true" :show.sync="tagShow"></tag-editor>
+        <noti v-ref:noti></noti>
+        <!-- <fail text="玲玉不让你看"></fail> -->
+        <change-password v-ref:changepassword></change-password>
+    </div>
 </template>
-
 <script>
 import fail from '../components/fail.vue'
 import tagEditor from '../components/publish/publish-tag-editor.vue'
 import noti from '../components/noti.vue'
+import changePassword from '../components/change-password.vue'
 module.exports = {
-    components:{
-        tagEditor,noti,fail
+    components: {
+        tagEditor,
+        noti,
+        fail,
+        changePassword
     },
-    methods:{
-      showNoti(type){
-        var that = this;
-        switch(type){
-          case 'alert':
-          this.$refs.noti.alert('不行啊',{
-            bgColor:'green',
-            callback(result,vm){
-              console.log('alert',result,vm);
+    methods: {
+        showChangePassword() {
+            this.$refs.changepassword.show({
+                validator(oldPwd, newPwd1, newPwd2) {
+                    console.log(oldPwd, newPwd1, newPwd2);
+                    return newPwd1===newPwd2?null:'两个新密码不一样';
+                },
+                confirm(oldPwd,newPwd) {
+                    alert(`旧密码:${oldPwd},新密码:${newPwd}`);
+                    return true;
+                }
+            })
+        },
+        showNoti(type) {
+            var that = this;
+            switch (type) {
+                case 'alert':
+                    this.$refs.noti.alert('不行啊', {
+                        bgColor: 'green',
+                        callback(result, vm) {
+                            console.log('alert', result, vm);
+                        }
+                    });
+                    break;
+                case 'confirm':
+                    this.$refs.noti.confirm('CONFIRM！你确认要打小泽？', {
+                        callback(result, vm) {
+                            console.log('confirm', result);
+                            if (result) vm.alert('打死他!');
+                            else vm.warning('不打不行!', {
+                                timeout: 2000
+                            });
+                        }
+                    });
+                    break;
+                case 'warning':
+                    this.$refs.noti.warning('WARNING!不可以这样的！', {
+                        callback(result, vm) {
+                            console.log('warning', result, vm);
+                        }
+                    });
+                    break;
+                case 'noti':
+                    this.$refs.noti.noti('NOTI!!!!23333333333333sdagasdgasddsagasdgsagssadgsadgasdgasdgs时代嘎上的嘎说');
+                    break;
             }
-          });
-          break;
-          case 'confirm':
-           this.$refs.noti.confirm('CONFIRM！你确认要打小泽？',{
-            callback(result,vm){
-              console.log('confirm',result);
-              if(result)vm.alert('打死他!');
-              else vm.warning('不打不行!',{
-                timeout:2000
-              });
-            }
-          });
-          break;
-          case 'warning':
-          this.$refs.noti.warning('WARNING!不可以这样的！',{
-            callback(result,vm){
-              console.log('warning',result,vm);
-            }
-          });
-          break;
-          case 'noti':
-          this.$refs.noti.noti('NOTI!!!!23333333333333sdagasdgasddsagasdgsagssadgsadgasdgasdgs时代嘎上的嘎说');
-          break;
+        },
+        clickTag() {
+            this.$refs.tagEditor.showMe();
         }
-      },
-      clickTag(){
-        this.$refs.tagEditor.showMe();
-      }
     },
-    data(){
+    data() {
         return {
-            tag:'',
-            tags:[
-              {
+            tag: '',
+            tags: [{
                 "name": "饭堂",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "阿姨",
-                "value":321
-              },
-              {
+                "value": 321
+            }, {
                 "name": "老师",
-                "value":333
-              },
-              {
+                "value": 333
+            }, {
                 "name": "实训",
-                "value":111
-              },
-              {
+                "value": 111
+            }, {
                 "name": "饭堂2",
-                "value":222
-              },
-              {
+                "value": 222
+            }, {
                 "name": "阿2姨",
-                "value":1221213
-              },
-              {
+                "value": 1221213
+            }, {
                 "name": "老师2",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "实训2",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "饭堂3",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "阿姨3",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "老师3",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "实训3",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "饭堂4",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "阿姨4",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "老师4",
-                "value":123
-              },
-              {
+                "value": 123
+            }, {
                 "name": "实训4",
-                "value":123
-              }
-            ],
-            tagShow:false
+                "value": 123
+            }],
+            tagShow: false
         }
     }
 }
